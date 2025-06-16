d3fe2556eb5f1e52f81e24eb89f740806732afd1
Apna-project2003 <aryan61865#gmail.com>
2025-06-16 16:02:14 +0530

    Added frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './components/ui/componennts_lite/Navbar'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
