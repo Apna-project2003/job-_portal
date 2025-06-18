5b182f8c1a61fe7698f9d6ab21e1ecba5309bb1c
Apna-project2003 <214593185+Apna-project2003@users.noreply.github.com>
2025-06-18 18:31:38 +0530

    AppliedJob

--- frontend/src/components/ui/componennts_lite/shared/AppliedJob.jsx
+++ frontend/src/components/ui/componennts_lite/shared/AppliedJob.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Applied = () => {
+const AppliedJob = () => {
   return (
     <div>
       
@@ -8,4 +8,5 @@ const Applied = () => {
   )
 }
 
-export default Applied
+export default AppliedJob
+
