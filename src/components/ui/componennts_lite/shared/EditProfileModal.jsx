1a2d6334f6078f6734a1ec496479207636fb4d9e
Apna-project2003 <214593185+Apna-project2003@users.noreply.github.com>
2025-06-18 23:03:31 +0530

    EditProfileModal

--- src/components/ui/componennts_lite/shared/EditProfileModal.jsx
+++ src/components/ui/componennts_lite/shared/EditProfileModal.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../../dialog'
+import { Label } from '../../label'
 
 const EditProfileModal = ({open,setOpen}) => {
   return (
     <div>
-      <Dialog>
+      <Dialog open={open} setOpen={setOpen}>
   <DialogTrigger>Open</DialogTrigger>
   <DialogContent>
     <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
+      <DialogTitle>Edit Profile</DialogTitle>
+    
     </DialogHeader>
+    <form>
+        <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+
+                <input
+                  type="text"
+                  id="name"
+                //   value={input.fullname}
+                  name="name"
+                //   onChange={changeEventHandler}
+                  className="col-span-3 border border-gray-300 rounded-md p-2"
+                />
+            </div>
+        </div>
+    </form>
   </DialogContent>
 </Dialog>
     </div>
